0a7f18e009bed1425a8091901bf5dbb5aa986091
VivianYWY <31574124+VivianYWY@users.noreply.github.com>
2025-12-23 14:46:56 +0800

    Update example2.py

--- example2.py
+++ example2.py
@@ -5,3 +5,10 @@ from langgraph.graph.message import add\_messages
   
 class SubAgentState(TypedDict):  
     messages: Annotated[Sequence[AnyMessage], add\_messages]
+
+
+from dotenv import load\_dotenv  
+load\_dotenv()  
+  
+from langchain.chat\_models import init\_chat\_model  
+llm = init\_chat\_model("deepseek-chat", model\_provider="deepseek")
