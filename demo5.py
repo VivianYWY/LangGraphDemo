c0c95683a372eac75a7efc8b2fb568e8d663e9bc
VivianYWY <31574124+VivianYWY@users.noreply.github.com>
2026-03-09 08:55:28 +0800

    Update demo5.py

--- demo5.py
+++ demo5.py
@@ -34,3 +34,25 @@ def save_text(content: str, path: str) -> str:
 class PromptSaveInput(BaseModel):
     content: str = Field(description="需要保存的内容")
     path: str = Field(description="保存内容的目录")
+
+# 定义发送电子邮件的函数
+def post_message(title: str, content: str, address: str) -> str:
+    """
+    发送电子邮件的函数
+    :param title: 邮件的主题
+    :param content: 邮件的内容
+    :param address: 邮箱地址
+    :return: 发送结果的字符串
+    """
+    # 第三方 SMTP 服务
+    mail_host = "smtp.163.com"  # SMTP服务器
+    mail_user = "1780105****@163.com"  # 用户名
+    mail_pass = "****"  # 授权密码, 非登录密码 写自己的
+
+    sender = '1780105****@163.com'  # 发件人邮箱(最好写全, 不然会失败)
+    receivers = [,address]  # 接收邮件, 可设置为你的QQ邮箱或者其他邮箱
+
+    message = MIMEText(content, 'plain', 'utf-8')  # 内容, 格式, 编码
+    message['From'] = "{}".format(sender)
+    message['To'] = ",".join(receivers)
+    message['Subject'] = title
